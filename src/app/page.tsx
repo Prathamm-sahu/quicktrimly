28af6dc4451d20e3d70eea7e12974a07ef3d6513
Pratham Sahu <prathamsahu051@gmail.com>
2024-02-08 12:14:34 +0530

    imported navbar and card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Card from "@/components/Card";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+    <main className="min-h-screen">
+      <div className="pl-52 pr-52 pt-44">
+        <Card />
+      </div>
     </main>
   );
 }
